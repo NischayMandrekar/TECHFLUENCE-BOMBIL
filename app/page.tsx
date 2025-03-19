08ad9d82b9d79cb6ea0ba759c4acd9d738842bd5
NischayMandrekar <mandrekarnischay91@gmail.com>
2025-03-19 20:21:46 +0530

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,77 @@
+"use client";
+import useUser from "@/hooks/useUser";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { fullName, setFullName } = useUser();
+  const [roomID, setRoomID] = useState("");
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    setFullName("");
+  }, []);
+
+  return (
+    <div className="w-full h-screen">
+      <section className="bg-gray-950 text-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 flex-col gap-24 flex h-screen items-center">
+          <Image src="/logo.svg" alt="logo" width={200} height={200} />
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text  font-extrabold text-transparent text-5xl">
+              {`Have a smooth meeting`}
+            </h1>
+            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text font-extrabold text-transparent text-5xl">
+              <span className="block">with team members</span>
+            </h1>
+            <p className="mx-auto mt-6 max-w-fit sm:text-xl/relaxed text-center">
+              Welcome to SignSync – the AI-powered real-time sign language
+              interpreter for virtual meetings. Our platform seamlessly converts sign language gestures into instant captions and speech, ensuring clear and inclusive communication for both deaf and hearing participants. Powered by advanced AI, real-time hand tracking,SignSync makes every virtual meeting accessible and effortless."
+            </p>
+            <div className="flex items-center justify-center gap-4 mt-6">
+              <input
+                type="text"
+                id="name"
+                onChange={(e) => setFullName(e.target.value.toString())}
+                className="border bg-white rounded-md focus:border-transparent focus:outline-none focus:ring-0 px-4 py-2 w-full text-black"
+                placeholder="Enter your name"
+              />
+            </div>
+
+            {fullName && fullName.length >= 3 && (
+              <>
+                <div className="flex items-center justify-center gap-4 mt-6">
+                  <input
+                    type="text"
+                    id="roomid"
+                    value={roomID}
+                    onChange={(e) => setRoomID(e.target.value)}
+                    className=" bg-white border rounded-md focus:border-transparent focus:outline-none focus:ring-0 px-4 py-2 w-full text-black"
+                    placeholder="Enter room ID to join a meeting"
+                  />
+                  <button
+                    className="rounded-md bg-blue-600 px-10 py-[11px] text-sm font-medium text-white focus:outline-none sm:w-auto"
+                    onClick={() => router.push(`/room/${roomID}`)}
+                    disabled={!roomID}
+                  >
+                    Join
+                  </button>
+                </div>
+                <div className="mt-4 flex items-center justify-center">
+                  <button
+                    className="text-lg font-medium hover:text-blue-400 hover:underline"
+                    onClick={() => router.push(`/room/${uuid()}`)}
+                  >
+                    Or create a new meeting
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
     </div>
   );
 }
